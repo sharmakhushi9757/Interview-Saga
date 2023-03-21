805208e9891e3550ce2760404d7c74d23bccae2a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-21 20:53:57 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 def main():
-    #im = Image.open("")
+
     st.set_page_config(
     page_title="INTERVIEW SAGA",
     page_icon=":male-technologist:",

d4cd3fcfab8a98920895e784218164c1bef8cd21
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-21 20:39:19 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -5,7 +5,7 @@ def main():
     #im = Image.open("")
     st.set_page_config(
     page_title="INTERVIEW SAGA",
-    page_icon="🧊",
+    page_icon="https://www.flaticon.com/free-icon/interview_3222637?term=interview&page=1&position=3&origin=search&related_id=3222637",
     layout="centered",
     initial_sidebar_state="auto",
     menu_items={

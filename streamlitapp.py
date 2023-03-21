8587fada27bc075c8365ee3ea14e0da7c194d04a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-21 20:24:05 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
 def main():
-    st.title("My Streamlit App")
+    
+    st.set_page_config(
+    page_title="INTERVIEW SAGA",
+    page_icon="👀",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'Get Help': 'https://github.com/sharmakhushi9757',
+        'Report a bug': "https://github.com/sharmakhushi9757",
+        'About': "sharmakhushi9757"
+    }
+)
+    
     st.write("Welcome to my app!")
 
 if __name__ == "__main__":

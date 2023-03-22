6491bb0fc75d6a90fb3116fbbd3f9755c4c57b6b
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-22 21:28:40 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -15,7 +15,21 @@ def main():
 )
     
     st.title("INTERVIEW SAGA")
-    st.write("Welcome to my app!")
-
+    st.markdown("# HOME PAGE ")
+    st.sidebar.markdown("# Main page ")
+    def page2():
+        st.markdown("# Page 2 ❄️")
+        st.sidebar.markdown("# Page 2 ❄️")
+    def page3():
+        st.markdown("# Page 3 🎉")
+        st.sidebar.markdown("# Page 3 🎉")
+    page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+    page_names_to_funcs[selected_page]()
+   
 if __name__ == "__main__":
     main()

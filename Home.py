0bac112baf9e4b382833dd68d688800e14fb81ff
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 01:37:05 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,46 +16,6 @@ st.set_page_config(
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
 
-im1=Image.open('Images/home.png')
-st.multipage_config(page_title="Home",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im1,
-ordering=1
-)
-
-im=Image.open('Images/prep.png')
-st.multipage_config(page_title="Interview Prep",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im,
-ordering=2
-)
-
-im2=Image.open('Images/resume.png')
-st.multipage_config(page_title="Resume",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im2,
-ordering=3
-)
-
-im3=Image.open('Images/mockiw.png')
-st.multipage_config(page_title="Mock Interview",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im3,
-ordering=4
-)
-
-im4=Image.open('Images/about.png')
-st.multipage_config(page_title="About",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im4,
-ordering=5
-)
-
 if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
 

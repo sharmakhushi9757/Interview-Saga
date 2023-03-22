52bdb81c75dc8823fc49a2482046baadb1595627
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-22 22:49:11 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,49 +1,18 @@
 import streamlit as st
-from PIL import Image
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
-
-# Contents of ~/my_app/pages/page_2.py
-import streamlit as st
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
-
-# Contents of ~/my_app/pages/page_3.py
-import streamlit as st
-
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
-'''
-def main():
-    st.set_page_config(
-    page_title="INTERVIEW SAGA",
-    page_icon="🧊",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items={
-        'Get Help': 'https://github.com/sharmakhushi9757',
-        'Report a bug': "https://github.com/sharmakhushi9757",
-        'About': "sharmakhushi9757"
-    }
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
 )
-    
-    st.title("INTERVIEW SAGA")
-    st.sidebar.markdown("# HOME PAGE ")
-    def HOME_PAGE():
-        st.markdown("# HOME APGE 🎈")
-        st.sidebar.markdown("# HOME PAGE 🎈")
-    def page2():
-        st.markdown("# Page 2 ❄️")
-        st.sidebar.markdown("# Page 2 ❄️")
-    def page3():
-        st.markdown("# Page 3 🎉")
-        st.sidebar.markdown("# Page 3 🎉")
-    page_names_to_funcs = {
-    "HOME PAGE": HOME_PAGE,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-    page_names_to_funcs[selected_page]()'''
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)

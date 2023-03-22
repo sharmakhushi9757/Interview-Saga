82c5e5056e98dd2b5ac2f585e729afe62be9bfa4
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-22 21:38:36 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,29 +1,20 @@
 import streamlit as st
 from PIL import Image
 
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
 
+# Contents of ~/my_app/pages/page_2.py
+import streamlit as st
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
 
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
+# Contents of ~/my_app/pages/page_3.py
+import streamlit as st
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
-
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
 '''
 def main():
     st.set_page_config(

77388a8edcc8ab3b8b0715f35e37662156825c36
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 01:21:56 +0530

    Update 2 Interview Prep.py

--- pages/2 Interview Prep.py
+++ pages/2 Interview Prep.py
@@ -1,12 +1,6 @@
 import streamlit as st
 from PIL import Image
-im=Image.open('Images/prep.png')
-st.set_page_config(page_title="Interview Prep",
-layout="wide",
-initial_sidebar_state="auto",
-multi_app_icon=im,
-ordering=1
-)
+
 
 st.title("Projects")
 

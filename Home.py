ff6917893e04b508f038454d177cc536b968b56a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 00:22:56 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open('Images/question.png')
+image = Image.open('Images/page icon.png')
 st.set_page_config(
     page_title="INTERVIEW SAGA",
     page_icon=image,

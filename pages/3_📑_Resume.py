4c4a43689f367a806505a791405447660694f514
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 18:48:35 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -93,24 +93,29 @@ with tab2:
    data="When you upload your resume, our AI-powered system analyzes it against key criteria required as per the Industry standards and commit an overall score to your resume. Our resume checker it gives you the fair idea of the resume quality and how it represents you in front of recruiters & hiring manager. We also provide you a detailed resume review and ways to increase the score further. You can also reach out to our professional resume experts to perfect your resume score. "
    st.markdown(data)
    st.image("Images/rr.png")
-
-
+    
    uploaded_file = st.file_uploader("Upload Resume")
    if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
       
 
         # To convert to a string based IO:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
         
 
         # To read file as string:
         string_data = stringio.read()
+        st.write(string_data)
        
 
         # Can be used wherever a "file-like" object is accepted:
         dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+    
         
     
   

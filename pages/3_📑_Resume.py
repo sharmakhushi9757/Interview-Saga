35e41e3e0ad472e5b5e204d0dc70426839be3626
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 16:35:37 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -1,5 +1,62 @@
 import streamlit as st
 
+image = Image.open('Images/page icon.png')
+st.set_page_config(
+    page_title="INTERVIEW SAGA",
+    page_icon=image,
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'Get Help': 'https://github.com/sharmakhushi9757',
+        'Report a bug': "https://github.com/sharmakhushi9757",
+        'About': "sharmakhushi9757"
+    }
+)
+
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(png_file):
+    with open(png_file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def build_markup_for_logo(
+    png_file,
+    background_position="10% 10%",
+    margin_top="10%",
+    image_width="30%",
+    image_height="",
+):
+    binary_string = get_base64_of_bin_file(png_file)
+    return """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url("data:image/png;base64,%s");
+                    background-repeat: no-repeat;
+                    background-position: %s;
+                    margin-top: %s;
+                    background-size: %s %s;
+                }
+            </style>
+            """ % (
+        binary_string,
+        background_position,
+        margin_top,
+        image_width,
+        image_height,
+    )
+
+
+def add_logo(png_file):
+    logo_markup = build_markup_for_logo(png_file)
+    st.markdown(
+        logo_markup,
+        unsafe_allow_html=True,
+    )
+
+add_logo("Images/logo1.png")
+
 
 
 st.title("We will help you to build your resume")

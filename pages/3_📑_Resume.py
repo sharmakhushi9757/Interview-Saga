a922063017efbd54ba0332ddc70cf7bc0ac18a3d
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 16:45:17 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -60,6 +60,21 @@ def add_logo(png_file):
 
 add_logo("Images/logo1.png")
 
+import streamlit as st
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
 st.title("We will help you to build your resume")

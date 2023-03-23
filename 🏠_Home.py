db7fa1904e9c9f6263e0104afc56cbc3f7c79f55
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 13:54:21 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -16,11 +16,14 @@ st.set_page_config(
 )
 
 def add_logo(logo_path, width, height):
+    """Read and return a resized logo"""
     logo = Image.open('Images/logo1.png')
     modified_logo = logo.resize((width, height))
     return modified_logo
 
-st.sidebar.image(add_logo(logo_path="Images/logo1.png", width=50, height=60))
+my_logo = add_logo(logo_path='Images/logo1.png', width=50, height=60)
+st.sidebar.image(my_logo)
+
 
 st.title("Main Page")
 st.sidebar.success("Select a page above.")

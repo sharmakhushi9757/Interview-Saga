e8ce16419451608d8628465ed5c943197182f5aa
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 18:03:37 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -86,28 +86,22 @@ with tab2:
    if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+      
 
         # To convert to a string based IO:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+        
 
         # To read file as string:
         string_data = stringio.read()
-        st.write(string_data)
+       
 
         # Can be used wherever a "file-like" object is accepted:
         dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+        
     
   
-   uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-   for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
   
-
 
 
 

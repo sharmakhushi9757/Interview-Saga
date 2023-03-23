24160ad4906cfd6f2abc83518736f0a9e8fa4ae4
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 18:51:07 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -98,12 +98,12 @@ with tab2:
    if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+        
       
 
         # To convert to a string based IO:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+        
 
         
 
@@ -114,7 +114,7 @@ with tab2:
 
         # Can be used wherever a "file-like" object is accepted:
         dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+        
     
         
     

15100ca10ea405a3f81d6aaafd97814b2d37dcf5
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 13:28:39 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,31 @@
 import streamlit as st
 from PIL import Image
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(http://placekitten.com/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 
 image = Image.open('Images/page icon.png')
+add_logo()
 st.set_page_config(
     page_title="INTERVIEW SAGA",
     page_icon=image,

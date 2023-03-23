cf018704eca42365f873304999499913637f2435
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 13:32:29 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,5 +1,21 @@
 import streamlit as st
 from PIL import Image
+
+
+image = Image.open('Images/page icon.png')
+add_logo()
+st.set_page_config(
+    page_title="INTERVIEW SAGA",
+    page_icon=image,
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'Get Help': 'https://github.com/sharmakhushi9757',
+        'Report a bug': "https://github.com/sharmakhushi9757",
+        'About': "sharmakhushi9757"
+    }
+)
+
 def add_logo():
     st.markdown(
         """
@@ -24,19 +40,9 @@ def add_logo():
     )
 
 
-image = Image.open('Images/page icon.png')
+
 add_logo()
-st.set_page_config(
-    page_title="INTERVIEW SAGA",
-    page_icon=image,
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items={
-        'Get Help': 'https://github.com/sharmakhushi9757',
-        'Report a bug': "https://github.com/sharmakhushi9757",
-        'About': "sharmakhushi9757"
-    }
-)
+
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
 

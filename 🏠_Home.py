4a4c0c76f631c20c7060bfec7dd01cd28fea6c11
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 16:03:35 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -62,7 +62,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('logo1.png')
+set_png_as_page_bg('Images/logo1.png')
 
 st.title("Main Page")
 st.sidebar.success("Select a page above.")

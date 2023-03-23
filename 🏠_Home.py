73b7229b7270c857b589df37e3d68b1716a03b6d
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 16:24:21 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -26,7 +26,7 @@ def get_base64_of_bin_file(png_file):
 
 def build_markup_for_logo(
     png_file,
-    background_position="50% 10%",
+    background_position="10% 50%",
     margin_top="10%",
     image_width="30%",
     image_height="",

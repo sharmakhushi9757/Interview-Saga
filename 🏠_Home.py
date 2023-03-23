8799613a23940133555b43012292852e36dbe01e
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 15:57:33 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,7 +14,29 @@ st.set_page_config(
         'About': "sharmakhushi9757"
     }
 )
-st.sidebar.image("Images/logo1.png", use_column_width=True)
+
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(http://placekitten.com/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 st.title("Main Page")
 st.sidebar.success("Select a page above.")

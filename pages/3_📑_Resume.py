2e789d143d66af2d44a80598c804651ddfdd385a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 17:26:27 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -76,7 +76,7 @@ with tab2:
    st.header("We will Score your Resume.")
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
    
-   col1, col2 = st.columns([3, 3])
+   col1, col3 ,col2, = st.columns([3,2,3])
    data = np.random.randn(10, 1)
 
    col1.subheader("A wide column with a chart")

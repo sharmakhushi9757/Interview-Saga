e4c6723240a65767f19ef7cc36f30536b3d59056
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-23 13:40:11 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -20,19 +20,12 @@ def add_logo():
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url(http://placekitten.com/200/200);
+                background-image: logo1;
                 background-repeat: no-repeat;
                 padding-top: 120px;
                 background-position: 20px 20px;
             }
-            [data-testid="stSidebarNav"]::before {
-                content: "iNTERVIEW SAGA";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 30px;
-                position: relative;
-                top: 100px;
-            }
+            
         </style>
         """,
         unsafe_allow_html=True,

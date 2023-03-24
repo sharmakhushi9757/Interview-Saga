a8a0dead6412f92cd3726998450cff6c13d7fc24
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 14:45:17 +0530

    Update 5_💠_About Us.py

--- pages/5_💠_About Us.py
+++ pages/5_💠_About Us.py
@@ -92,5 +92,12 @@ contact_form = """
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
 
 

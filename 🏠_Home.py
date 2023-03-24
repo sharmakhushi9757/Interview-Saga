ed362d7d71519abb3fb8b45a54e3cb686aa7e832
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 22:12:22 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -69,28 +69,12 @@ st.subheader(":blue[Now!! All available at one place]")
 st.sidebar.success("Select a page above.")
 st.image("Images/backk.png")
 
-import time
-import requests
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
 
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-
-lottie_url_hello = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
-lottie_url_download = "https://assets4.lottiefiles.com/private_files/lf30_t26law.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-lottie_download = load_lottieurl(lottie_url_download)
-
-
-st_lottie(lottie_hello, key="hello")
-
-if st.button("Download"):
-    with st_lottie_spinner(lottie_download, key="download"):
-        time.sleep(5)
-    st.balloons()
+cc1,cc2,cc3=st.columns(3)
+with cc1:
+    st.image("https://pin.it/6YIKHlm",caption="Practice Set")
+with cc2:
+    st.image("https://pin.it/i2D1dXL ",caption="Resume Building")
+with cc3:
+    st.image(" https://pin.it/2MxxZP5",caption="Mock Interview")
+    

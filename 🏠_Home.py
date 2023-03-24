166a4b4088a558d982db3fe43c440cc01d80b638
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 21:05:37 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -65,15 +65,13 @@ add_logo("Images/logo1.png")
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
 
-with st.container():
-   page_bg_img = '''
+page_bg_img = '''
     <style>
         body {
         background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
         background-size: cover;
         }
     </style>
-    '''
-   
-   
-   st.markdown(page_bg_img, unsafe_allow_html=True)
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)

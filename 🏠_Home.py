77ff36beb18d9abe6f24e17741a965eba463fe6f
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 21:31:53 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -63,7 +63,7 @@ add_logo("Images/logo1.png")
 page_bg="""
     <style>
         [data-testid="stAppViewContainer"]{
-        background-color:;
+        background-color:#ADD8E6;
         }
         </style>"""
 st.markdown(page_bg,unsafe_allow_html=True)

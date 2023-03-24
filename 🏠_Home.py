f0006f75e767f105f8437ebc443abd935c33a36e
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 21:38:50 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -67,3 +67,27 @@ st.header("Everything you need to crack your next interview")
 st.subheader(":blue[Now!! All available at one place]")
 st.sidebar.success("Select a page above.")
 st.image("Images/backk.png")
+
+
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 2))
+
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
+
+    new_rows = np.random.randn(10, 2)
+
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
+
+    # Append data to the chart.
+    chart.add_rows(new_rows)
+
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
+
+status_text.text('Done!')
+st.balloons()

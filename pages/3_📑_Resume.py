15d0d2aade4f072532dd73d376adfafb21e9409a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-24 15:20:27 +0530

    Update 3_📑_Resume.py

--- pages/3_📑_Resume.py
+++ pages/3_📑_Resume.py
@@ -77,7 +77,7 @@ with tab1:
    
    resume_data="For most job-seekers, a good resume is what stands between a dream job and Choice D. Get your resume right, and you’ll be getting replies from every other company you apply to.If your resume game is weak, though, you’ll end up sitting around for weeks, maybe even months, before you even get a single response.So you’re probably wondering how you can write a resume that leads to HR managers inviting you to interviews daily.Well, you’ve come to the right place!In this guide, we’re going to teach you everything you need to know about how to make a resume, including:"
    st.markdown(resume_data)
-   st.subheader("How to Write a Resume in 9 Steps:")
+   st.subheader("✔️ How to Write a Resume in 9 Steps:")
    st.write("1. Pick the Right Resume Format & Layout")
    st.write("2. Mention Your Personal Details & Contact Information")
    st.write("3. Use a Resume Summary or Objective")

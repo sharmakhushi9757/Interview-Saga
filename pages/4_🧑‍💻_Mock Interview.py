c6bf17a0fa5bcdf7aec689b62d4ae2b85a43eda6
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-03-26 16:59:30 +0530

    Update 4_🧑‍💻_Mock Interview.py

--- pages/4_🧑‍💻_Mock Interview.py
+++ pages/4_🧑‍💻_Mock Interview.py
@@ -76,5 +76,15 @@ with st.form("Book Your Interview Here!!",clear_on_submit=True):
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       st.write("Congrts!! you have booked your interview on :" ,sch,"at",tm,"You will receive further details vai mail.")
+       st.write("Congrts!! you have booked your interview on :" ,sch,"at",tm,"You will receive further details via mail.")
+       st.write("**Fill this form for conformtion :**")
+        with st.form("details",clear_on_submit=True):
+            Highest Education = st.text_input("Enter Highest Qualification :")
+            Experience = st.number_input("Enter Experience (in numbers) :")
+            Role = st.selectbox('Select Role',('Software Developer', 'Data Science', 'Data Analytics','Python Developer','Java Developer'))
+            book=st.form_submit_button("Book")
+        
+
+    
+            
 
